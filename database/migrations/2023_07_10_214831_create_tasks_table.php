1d6c042ed7c63e70086b30c410c7aa432347e46b
Luiz Henrique Santos <luiz.santos85@gmail.com>
2023-07-11 21:51:36 -0300

    implementando as migrations e criando os models

---
+++ database/migrations/2023_07_10_214831_create_tasks_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->datetime('due_date');
+            $table->text('description');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Excluir o relacionamento
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+            $table->dropForeign(['user_id']);
+        });
+        
+        Schema::dropIfExists('tasks');
+    }
+};
