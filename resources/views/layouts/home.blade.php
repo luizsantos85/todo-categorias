1a4694bfa1b6d313682f1686ffab57e42e8d07e0
Luiz Henrique Santos <luiz.santos85@gmail.com>
2023-07-18 22:18:53 -0300

    ajuste layout css

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -22,10 +22,59 @@
             </nav>
             <main>
                 <section class="graph">
-                    Grafico
+                    <div class="graph_header">
+                        <h2>Progresso do dia</h2>
+                        <hr class="line_graph_header"/>
+                        Data
+                    </div>
+
+                    <div class="subtitle_graph_header">
+                        Tarefas:
+                        <b>3/7</b>
+                    </div>
+
+                    <div class="graph_body"></div>
+
+                    <div class="graph_footer">
+                        <p>Restam 4 tarefas para serem feitas.</p>
+                    </div>
                 </section>
+
                 <section class="list">
-                    Lista
+                    <div class="list_header">
+                        <select name="" id="" class="list_header_select">
+                            <option value="">Selecione...</option>
+                            <option value="">Todas as tarefas</option>
+                        </select>
+                    </div>
+                    <div class="task_list">
+                        <div class="task">
+                            <div class="task_title">
+                                <input type="checkbox" name="" id="" />
+                                <span>Título da tarefa </span>
+                            </div>
+                            <div class="task_color">
+                                <div class="priority_color"></div>
+                                <span>Prioridade </span>
+                            </div>
+                            <div class="task_buttons">
+                                Editar - Excluir
+                            </div>
+                        </div>
+                        <div class="task">
+                            <div class="task_title">
+                                <input type="checkbox" name="" id="" />
+                                <span>Título da tarefa </span>
+                            </div>
+                            <div class="task_color">
+                                <div class="priority_color"></div>
+                                <span>Prioridade </span>
+                            </div>
+                            <div class="task_buttons">
+                                Editar - Excluir
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
         </div>
